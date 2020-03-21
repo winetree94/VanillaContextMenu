4e97722fac4abb880d5a0068876ecb2de66252bd
winetree94 <winetree94@outlook.com>
2020-03-21 20:14:04 +0900

    20200321

--- stories/demo.stories.ts
+++ stories/demo.stories.ts
@@ -9,71 +9,22 @@ import 'bootstrap/dist/js/bootstrap.min';
 import '../src/styles/style.css';
 import { VanillaContext } from '../src/Container';
 
-const stories = storiesOf('Intro', module);
+const stories = storiesOf('Intro', exports);
 
-stories.add('intro', () => {
-  const table = document.createElement('table');
-  table.className = 'pure-table';
-  table.innerHTML = `
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
+stories.add('what is', () => {
+  const container = document.createElement('div');
+  container.className = 'container-fluid';
+  container.style.paddingTop = '20px';
+  container.innerHTML = `
+  <div class="jumbotron">
+  <h1>Pure context menu for modern web browser!</h1>
+  <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing
+  responsive, mobile-first projects on the web.</p>
+  </div>
+  <p>This is some text.</p>
+  <p>This is another text.</p>
   `;
-  const contextmenu = new VanillaContext(table, {
-    itemStyle: {
-      height: '10px'
-    },
-    nodes: [
-      {
-        renderer: '<i></i>Hello World',
-        onClick: params => action('click'),
-        children: [
-          {
-            renderer: 'hello',
-            style: {
-              height: '50px'
-            }
-          },
-          {
-            renderer: 'hello'
-          }
-        ]
-      }
-    ]
-  });
-  return table;
+  return container;
 });
 
 // stories.add('backup', () => {
